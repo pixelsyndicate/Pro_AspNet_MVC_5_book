d462cf057bb22ea69455560b164cdb6a036b741e
pixelsyndicate@hotmail.com <pixelsyndicate@hotmail.com>
2015-09-15 10:45:06 -0500

    first hello world
    following the examples in the book, up to page 24, chapter 2

---
+++ Code/ProAspNetMvc5/firstMvcApp/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace firstMvcApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+            return View();
+        }
+
+        public string Index2()
+        {
+            return "Hello World";
+        }
+
+        public ActionResult Index3()
+        {
+            return View();
+        }
+    }
+}
