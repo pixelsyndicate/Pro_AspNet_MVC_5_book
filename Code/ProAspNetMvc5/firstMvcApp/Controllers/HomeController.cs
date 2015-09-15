1d36feefa1c720f72d31158dd29791ea6809729f
pixelsyndicate@hotmail.com <pixelsyndicate@hotmail.com>
2015-09-15 12:09:17 -0500

    adding bootstrap, validation and form result response up to chapter 2, page 43

--- Code/ProAspNetMvc5/firstMvcApp/Controllers/HomeController.cs
+++ Code/ProAspNetMvc5/firstMvcApp/Controllers/HomeController.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
+using firstMvcApp.Models;
 
 namespace firstMvcApp.Controllers
 {
@@ -17,6 +15,26 @@ namespace firstMvcApp.Controllers
             return View();
         }
 
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                // todo: email guest response to the party organizer
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                // there is a validation error
+                return View();
+            }
+        }
+
         public string Index2()
         {
             return "Hello World";
@@ -26,5 +44,7 @@ namespace firstMvcApp.Controllers
         {
             return View();
         }
+
+
     }
 }
