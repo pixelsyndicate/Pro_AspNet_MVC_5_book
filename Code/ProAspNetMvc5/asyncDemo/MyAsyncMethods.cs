ca4ffbdb24303253caae4fcff03220539836f439
pixelsyndicate@hotmail.com <pixelsyndicate@hotmail.com>
2015-09-16 16:12:34 -0500

    Added console app to test Async and Await

---
+++ Code/ProAspNetMvc5/asyncDemo/MyAsyncMethods.cs
@@ -0,0 +1,46 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace asyncDemo
+{
+    public class MyAsyncMethods
+    {
+        // TASK<> is a threading task
+        public static Task<long?> GetPageLengthOldWay()
+        {
+            // create a mini web browser
+            HttpClient client = new HttpClient();
+
+            // HttpClient.GetAsync returns Task<HttpResponseMessage>
+            Task<HttpResponseMessage> httpTask = client.GetAsync("http://apress.com");
+
+            // we could do other things were while waiting for the HTTP request to complete
+
+            // continueWith means only continue when the task completes (antecedent means a thing that was preexisting or previous)
+            return httpTask.ContinueWith((Task<HttpResponseMessage> antecedent) =>
+            {
+                return antecedent.Result.Content.Headers.ContentLength; // odd. we have two returns?
+            });
+
+            // or we can do it with Lambda
+            return httpTask.ContinueWith((Task<HttpResponseMessage> antecedent) => antecedent.Result.Content.Headers.ContentLength);
+        }
+
+
+        // applying async and await keywords to simplify the ContinueWith conundrum
+        public async static Task<long?> GetPageLengthNewWay()
+        {
+            // create a mini web browser
+            HttpClient client = new HttpClient();
+
+            // await HttpClient.GetAsync returns HttpResponseMessage (no longer threading task containing it eg. Task<HttpResponseMessage>)
+            HttpResponseMessage httpMessage = await client.GetAsync("http://apress.com");
+
+            // we could do other things were while waiting for the HTTP request to complete
+
+            // now we don't need .ContinueWith((Task<HttpResponseMessage> antecedent) => antecedent.Result.Content.Headers.ContentLength)
+            return httpMessage.Content.Headers.ContentLength;
+
+        }
+    }
+}
---
+++ Code/ProAspNetMvc5/asyncDemo/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace asyncDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RunTest();
+
+            RunTest();
+
+            Console.ReadKey();
+        }
+
+        private static void RunTest()
+        {
+            Console.WriteLine("Testing stuff with Task<>");
+
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            var oldWayResult = GetApressPageLengthOldWay();
+            stopWatch.Stop();
+            Console.WriteLine(oldWayResult);
+
+            // Get the elapsed time as a TimeSpan value.
+            TimeSpan ts = stopWatch.Elapsed;
+
+            // Format and display the TimeSpan value.
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                ts.Hours, ts.Minutes, ts.Seconds,
+                ts.Milliseconds / 10);
+            Console.WriteLine(elapsedTime, "RunTime");
+            Console.WriteLine("---");
+
+            Console.WriteLine("Testing stuff with async / await");
+            stopWatch = new Stopwatch();
+            stopWatch.Start();
+            var newWayResult = GetApressPageLengthNewWay();
+            stopWatch.Stop();
+            Console.WriteLine(newWayResult);
+
+            // Get the elapsed time as a TimeSpan value.
+            ts = stopWatch.Elapsed;
+
+            // Format and display the TimeSpan value.
+            elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                ts.Hours, ts.Minutes, ts.Seconds,
+                ts.Milliseconds / 10);
+            Console.WriteLine(elapsedTime, "RunTime");
+            Console.WriteLine("---");
+        }
+
+        public static string GetApressPageLengthOldWay()
+        {
+            Task<long?> l = MyAsyncMethods.GetPageLengthOldWay();
+
+            // returns "The length of apress.com's Result.Content.Headers.ContentLength is 120287"
+            return string.Format("The length of apress.com's Result.Content.Headers.ContentLength is {0}", l.Result);
+        }
+
+        public static string GetApressPageLengthNewWay()
+        {
+            Task<long?> l = MyAsyncMethods.GetPageLengthNewWay();
+
+            // returns "The length of apress.com's Result.Content.Headers.ContentLength is 120287"
+            return string.Format("The length of apress.com's Result.Content.Headers.ContentLength is {0}", l.Result);
+        }
+
+
+    }
+
+
+}
