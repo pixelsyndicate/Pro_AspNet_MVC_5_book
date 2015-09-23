11d31b3cdef05ac5db89e8730a9eb922b48921b3
pixelsyndicate@hotmail.com <pixelsyndicate@hotmail.com>
2015-09-23 13:44:09 -0500

    added debug output to local iis, added a controller and a view for Product/List

--- Code/ProAspNetMvc5/Domain/Entities/Product.cs
+++ Code/ProAspNetMvc5/Domain/Entities/Product.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SportsStore.Domain.Entities
+﻿namespace SportsStore.Domain.Entities
 {
     public class Product
     {
---
+++ Code/ProAspNetMvc5/WebUI/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository _repo;
+
+        public ProductController(IProductRepository productRepo)
+        {
+            _repo = productRepo;
+        }
+
+        public ViewResult List()
+        {
+            return View(_repo.Products);
+        }
+
+    }
+}
