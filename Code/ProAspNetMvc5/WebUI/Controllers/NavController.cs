7deb0a965bbfab54fe6bc96bdf4132a1bd388551
pixelsyndicate@hotmail.com <pixelsyndicate@hotmail.com>
2015-10-14 13:47:36 -0500

    added another categories nav menu, one to use in small displays.

--- Code/ProAspNetMvc5/WebUI/Controllers/NavController.cs
+++ Code/ProAspNetMvc5/WebUI/Controllers/NavController.cs
@@ -1,43 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using SportsStore.Domain.Abstract;
 
 namespace SportsStore.WebUI.Controllers
 {
     /// <summary>
-    /// I will use Html.Action to inject action returns into the View
+    ///     I will use Html.Action to inject action returns into the View
     /// </summary>
     public class NavController : Controller
     {
-
-        private IProductRepository _repo;
+        private readonly IProductRepository _repo;
 
         public NavController(IProductRepository repo)
         {
             _repo = repo;
         }
 
-        public PartialViewResult Menu(string category = null)
+        public PartialViewResult Menu(string category = null, bool horizontalLayout = false)
         {
             // rather than hold the list of categories and the currently selected category in my viewmodel, i'm gonna cheat a bit and put it into a viewbag
             ViewBag.SelectedCategory = category;
 
             IEnumerable<string> categories = _repo.Products.Select(p => p.Category).Distinct().OrderBy(x => x);
 
-            // returning a partialview passes the model to a template PartialView.
-            return PartialView(categories);
-        }
+            // added to support different menu for small browsers
+            string viewName = horizontalLayout ? "MenuHorizontal" : "Menu";
+            return PartialView(viewName, categories);
 
-        /// <summary>
-        /// this is called a 'Child Action' - will be used on all pages.
-        /// </summary>
-        /// <returns></returns>
-        //public string Menu()
-        //{
-        //    return "Hello from NavController";
-        //}
+
+            // returning a partialview passes the model to a template PartialView.
+            // return PartialView(categories);
+
+        }
     }
 }
