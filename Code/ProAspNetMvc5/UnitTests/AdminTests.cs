285997d67b5201c9c5299d0a1fc3c1d9ade480c2
pixelsyndicate@hotmail.com <pixelsyndicate@hotmail.com>
2015-10-22 10:57:50 -0500

    added initial admin page

---
+++ Code/ProAspNetMvc5/UnitTests/AdminTests.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using SportsStore.WebUI.Controllers;
+using SportsStore.WebUI.Models;
+
+namespace SportsStore.UnitTests
+{
+    [TestClass]
+    public class AdminTests
+    {
+        [TestMethod]
+        public void Index_Contains_All_Products()
+        {
+            // arrange - create the mock repository
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new[]
+            {
+                new Product {ProductID = 1, Name = "p1"}, new Product {ProductID = 2, Name = "p2"},
+                new Product {ProductID = 3, Name = "p3"}
+            });
+
+            // arrange - create a controller
+            AdminController target = new AdminController(mock.Object);
+
+            // action
+            // example of what worked elsewhere - (CartIndexViewModel)target.Index(cart, "myUrl").ViewData.Model;
+          
+            Product[] results = ((IEnumerable<Product>)target.Index().ViewData.Model).ToArray();
+
+            // Assert
+            Assert.AreEqual(results.Length, 3);
+            Assert.AreEqual("p1", results[0].Name);
+            Assert.AreEqual("p2", results[1].Name);
+            Assert.AreEqual("p3", results[2].Name);
+        }
+    }
+}
---
+++ Code/ProAspNetMvc5/WebUI/Controllers/AdminController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SportsStore.Domain.Abstract;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class AdminController : Controller
+    {
+
+        private IProductRepository _repo;
+
+        public AdminController(IProductRepository repo)
+        {
+            _repo = repo;
+        }
+
+        // GET: Admin
+        public ViewResult Index()
+        {
+            return View(_repo.Products);
+        }
+
+       
+    }
+}
